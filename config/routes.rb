489292b205af0b18b1bd26dd7991867fdb145118
Ryosukeskmt <ryosuke.programing@gmail.com>
2020-06-01 07:52:06 +0900

    ログアウトと新規登録のflashを調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+    get 'confirm_email', to: 'users/registrations#confirm_email'
+  end
+
   get 'comments/create'
   get 'comments/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "mylesson" => "lessons#mylesson"
   root 'lessons#index'
   resources :lessons
